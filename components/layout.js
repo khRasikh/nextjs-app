f63ef38849fac2fbaa962b5114a497284bb3bf38
khudadad rasikh <rasikh@khudadads-mbp.speedport.ip>
2022-06-14 13:02:58 +0200

    Nextjs basic APP

---
+++ components/layout.js
@@ -0,0 +1,16 @@
+// import Nav from './Nav'
+// import Meta from './Meta'
+// import Header from './Header'
+import styles from "../styles/Layout.module.css";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <div className={styles.container}>
+        <main className={styles.main}>{children}</main>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
